ee6013eae8fc953a03cfa05f46fddf839f78caa2
Rich Gwozdz <rgwozdz@esri.com>
2023-08-21 15:57:53 -0700

    chore: covert to jest

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    process: 'readonly',
+    console: 'readonly',
+    __dirname: 'readonly',
+    fail: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  rules: {
+    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    indent: ['error', 2, {'SwitchCase': 1}],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-const fs = require('fs')
-const jwt = require('jsonwebtoken')
-const validateCredentials = require('./validate-credentials')
-const TOKEN_EXPIRATION_MINUTES = 60
-let _useHttp
-let _tokenExpirationMinutes
-let _secret
-let _userStoreFilePath
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const joi = require('joi');
+const { validateCredentials } = require('./validate-credentials');
+const errorPrefix = 'Auth plugin: ';
+const TOKEN_EXPIRATION_MINUTES = 60;
+
+const optionsSchema = joi.object({
+  useHttp: joi.boolean().default(false),
+  tokenExpirationMinutes: joi
+    .number()
+    .integer()
+    .greater(4)
+    .default(TOKEN_EXPIRATION_MINUTES),
+});
+
+let _useHttp;
+let _tokenExpirationMinutes;
+let _secret;
+let _userStoreFilePath;
 
 /**
  * configure auth functions
@@ -15,40 +27,43 @@ let _userStoreFilePath
  * @param {integer}  options.tokenExpirationMinutes - number of minutes until token expires
  * @param {boolean}  options.useHttp - direct consumers of authenticationSpecifcation to use HTTP instead of HTTPS
  */
-function auth (secret, userStoreFilePath, options = {}) {
+function initAuthPlugin(secret, userStoreFilePath, options = {}) {
   // Throw error if user-store file does not exist
-  fs.stat(userStoreFilePath, function (err, stats) {
-    if (err) throw err
-  })
+  const result = fs.existsSync(userStoreFilePath);
+  if (!result) {
+    throw new Error(`${errorPrefix}${userStoreFilePath} not found`);
+  }
+  _secret = secret;
+  _userStoreFilePath = userStoreFilePath;
 
-  _secret = secret
-  _userStoreFilePath = userStoreFilePath
+  const {
+    error,
+    value: { tokenExpirationMinutes, useHttp },
+  } = optionsSchema.validate(options);
 
-  // Ensure the useHttp option is a boolean and default to false
-  if (options.useHttp && typeof options.useHttp !== 'boolean') throw new Error(`"useHttp" must be a boolean`)
-  _useHttp = options.useHttp || false
+  if (error) {
+    throw new Error(`${errorPrefix}${error.details[0].message}`);
+  }
 
-  //  Ensure token expiration is an integer greater than 5
-  if (options.tokenExpirationMinutes && (!Number.isInteger(options.tokenExpirationMinutes) || options.tokenExpirationMinutes < 5)) throw new Error(`"tokenExpirationMinutes" must be an integer >= 5`)
-
-  _tokenExpirationMinutes = options.tokenExpirationMinutes || TOKEN_EXPIRATION_MINUTES
+  _useHttp = useHttp;
+  _tokenExpirationMinutes = tokenExpirationMinutes;
 
   return {
     type: 'auth',
     authenticationSpecification,
     authenticate,
-    authorize
-  }
+    authorize,
+  };
 }
 
 /**
  * Return "authenticationSpecification" object for use in output-services
  * @returns {object}
  */
-function authenticationSpecification () {
+function authenticationSpecification() {
   return {
-    useHttp: _useHttp
-  }
+    useHttp: _useHttp,
+  };
 }
 
 /**
@@ -56,34 +71,32 @@ function authenticationSpecification () {
  * @param {object} req Express request object
  * @returns {Promise}
  */
-function authenticate (req) {
-  return new Promise((resolve, reject) => {
-    let username
-    let password
-    if (req && req.query && req.query.username) username = req.query.username
-    if (req && req.query && req.query.password) password = req.query.password
+async function authenticate(req) {
+  const username = req.query?.username;
+  const password = req.query?.password;
 
-    // Validate user's credentials
-    validateCredentials(username, password, _userStoreFilePath)
-      .then(valid => {
-        // If credentials were not valid, reject
-        if (!valid) {
-          let err = new Error('Invalid credentials.')
-          err.code = 401
-          reject(err)
-        }
-        // Create access token and wrap in response object
-        let expires = Date.now() + (_tokenExpirationMinutes * 60 * 1000)
-        let json = {
-          token: jwt.sign({exp: Math.floor(expires / 1000), sub: username}, _secret),
-          expires
-        }
-        resolve(json)
-      })
-      .catch(err => {
-        reject(err)
-      })
-  })
+  // Validate user's credentials
+  const valid = await validateCredentials(
+    username,
+    password,
+    _userStoreFilePath,
+  );
+
+  if (!valid) {
+    let err = new Error('Invalid credentials.');
+    err.code = 401;
+    throw err;
+  }
+
+  // Create access token and wrap in response object
+  const expires = Date.now() + _tokenExpirationMinutes * 60 * 1000;
+  return {
+    token: jwt.sign(
+      { exp: Math.floor(expires / 1000), sub: username },
+      _secret,
+    ),
+    expires,
+  };
 }
 
 /**
@@ -91,28 +104,22 @@ function authenticate (req) {
  * @param {object} req Express request object
  * @returns {Promise}
  */
-function authorize (req) {
-  return new Promise((resolve, reject) => {
-    // Make sure there is token
-    let token
-    if (req && req.query && req.query.token) token = req.query.token
-    if (req && req.headers && req.headers.authorization) token = req.headers['authorization']
-    if (!token) {
-      let err = new Error('No authorization token.')
-      err.code = 401
-      reject(err)
-    }
-    // Verify token with async decoded function
-    jwt.verify(token, _secret, function (err, decoded) {
-      // If token invalid, reject
-      if (err) {
-        err.code = 401
-        reject(err)
-      }
-      // Resolve the decoded token (an object)
-      resolve(decoded)
-    })
-  })
+async function authorize(req) {
+  let token = req.query?.token || req.headers['authorization'];
+
+  if (!token) {
+    let err = new Error('No authorization token.');
+    err.code = 401;
+    throw err;
+  }
+  // Verify token with async decoded function
+  try {
+    const decoded = await jwt.verify(token, _secret);
+    return decoded;
+  } catch (err) {
+    err.code = 401;
+    throw err;
+  }
 }
 
-module.exports = auth
+module.exports = initAuthPlugin;
